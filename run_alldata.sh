6248f6fd1b479b46d2e09e3ba77387a11228e40e
Lydia Chau <lydia.chau@mail.utoronto.ca>
2021-12-12 15:07:20 -0500

    change sh file

--- run_alldata.sh
+++ run_alldata.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-python3 trainning_all --task amazon_google
-python3 trainning_all --task dirty_amazon_itunes
-python3 trainning_all --task dirty_dblp_acm
-python3 trainning_all --task dirty_dblp_scholar
-python3 trainning_all --task dirty_walmart_amazon
+srun --partition=smallgpunodes --mem=12G --gres=gpu:1 --pty bash -i
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pkgs_local/cuda-10.2/lib64
+export PATH=$PATH:/pkgs_local/cuda-10.2/bin
+
+python3 trainning_all.py --task amazon_google
+python3 trainning_all.py --task dirty_amazon_itunes
+python3 trainning_all.py --task dirty_dblp_acm
+python3 trainning_all.py --task dirty_dblp_scholar
+python3 trainning_all.py --task dirty_walmart_amazon
